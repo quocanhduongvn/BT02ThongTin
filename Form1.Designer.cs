94d3017b06a01cbaf21d849ed860c4c7d3a25636
Dương Quốc Anh <percyfire777@gmail.com>
2024-06-28 02:59:55 +0700

    master test commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,9 +50,10 @@
             // 
             this.lbHoVaTen.AutoSize = true;
             this.lbHoVaTen.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHoVaTen.ForeColor = System.Drawing.Color.Red;
             this.lbHoVaTen.Location = new System.Drawing.Point(12, 15);
             this.lbHoVaTen.Name = "lbHoVaTen";
-            this.lbHoVaTen.Size = new System.Drawing.Size(50, 12);
+            this.lbHoVaTen.Size = new System.Drawing.Size(63, 15);
             this.lbHoVaTen.TabIndex = 1;
             this.lbHoVaTen.Text = "Họ và Tên";
             this.lbHoVaTen.Click += new System.EventHandler(this.lbHoVaTen_Click);
@@ -61,6 +62,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
             this.label2.Location = new System.Drawing.Point(12, 44);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(58, 15);
@@ -78,9 +80,10 @@
             // 
             this.lbSothich.AutoSize = true;
             this.lbSothich.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSothich.ForeColor = System.Drawing.Color.Red;
             this.lbSothich.Location = new System.Drawing.Point(12, 75);
             this.lbSothich.Name = "lbSothich";
-            this.lbSothich.Size = new System.Drawing.Size(44, 12);
+            this.lbSothich.Size = new System.Drawing.Size(55, 15);
             this.lbSothich.TabIndex = 4;
             this.lbSothich.Text = "Sở Thích";
             this.lbSothich.Click += new System.EventHandler(this.lbSothich_Click);
@@ -98,6 +101,7 @@
             // btnXem
             // 
             this.btnXem.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXem.ForeColor = System.Drawing.Color.Red;
             this.btnXem.Location = new System.Drawing.Point(92, 93);
             this.btnXem.Name = "btnXem";
             this.btnXem.Size = new System.Drawing.Size(66, 23);
@@ -109,6 +113,7 @@
             // btnXoa
             // 
             this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.ForeColor = System.Drawing.Color.Red;
             this.btnXoa.Location = new System.Drawing.Point(164, 93);
             this.btnXoa.Name = "btnXoa";
             this.btnXoa.Size = new System.Drawing.Size(66, 23);
